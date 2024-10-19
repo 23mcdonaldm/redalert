0400ea1ec67e2b0934a5a2a58c0382ce2e592700
Milesm <miles1301@icloud.com>
2024-10-18 21:36:44 -0400

    added very basic comments, posting and getting comments, no styling done, have to figure out how to display usernames.

--- controllers/discussionController.js
+++ controllers/discussionController.js
@@ -15,14 +15,36 @@ module.exports.getDiscussionPosts = async (req, res) => {
     }
 }
 
-module.exports.getReplies = async (req, res) => {
-    const main_post = req.body;
+module.exports.getComments = async (req, res) => {
+    const main_post = req.body.disc_uid;
+    console.log(main_post);
     try {
         let getQuery = `SELECT * FROM discussion_post WHERE parent_post_uid = '${main_post}'`;
-        const response = await pool.query(getQuery);
-        res.status(200).json({response});
+        const response = await pool.query(getQuery);  
+        if (response.rows.length > 0) {
+            // If there are comments, send them back with a 200 status
+            res.status(200).json({ comments: response.rows });
+        } else {
+            // If no comments, send a different message or empty array
+            res.status(200).json({ comments: [], message: "No comments yet" });
+        }
     } catch (err) {
-        console.error("Couldn't get replies to post: " + err);
+        console.error("Couldn't get comments to post: " + err);
+    }
+}
+
+module.exports.postComment = async (req, res) => {
+    const { commentText, disc_uid, person_uid } = req.body;
+    console.log("parent post: " + disc_uid);
+    try {
+        let insertCommentQuery = `INSERT INTO discussion_post (post_uid, parent_post_uid, subject, timestamp, description, likes, dislikes, verified, person_uid, school_uid) 
+                                                    VALUES (uuid_generate_v4(), '${disc_uid}', 'comment', NOW(), '${commentText}', 0, 0, False, '${person_uid}', 
+                                                    (SELECT school_uid FROM person WHERE person_uid = '${person_uid}'))`;
+        const result = await pool.query(insertCommentQuery);
+        res.status(201).json({ message: 'Comment posted successfully', comment: result.rows[0] });
+    } catch (err) {
+        console.error('Error posting comment:', err);
+        res.status(500).json({ message: 'Failed to post the comment' });
     }
 }
 
@@ -30,8 +52,6 @@ module.exports.getDiscussionByUUID = async function(uuid) {
     if (!isUUID(uuid)) {
         return { error: "Invalid UUID format." };
     }
-    
-    
     try {
         let getPostQuery = `SELECT * FROM public_discussion_post WHERE disc_uid = '${uuid}'`;
         const { rows } = await pool.query(getPostQuery);
@@ -44,3 +64,4 @@ module.exports.getDiscussionByUUID = async function(uuid) {
         console.error("Couldn't find/get discussion post: " + err);
     }
 }
+
--- routes/discRoutes.js
+++ routes/discRoutes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const discussionController = require('../controllers/discussionController');
 const api = require('../utils/api.js');
 
-
+const BASE_URL = 'http://localhost:3000';
 const router = Router();
 
 router.post(`/getDiscussionPosts`, discussionController.getDiscussionPosts);
@@ -11,22 +11,37 @@ router.get('/discussion', (req, res) => {
     res.render('discussion');
 })
 
+router.post(`/getComments`, discussionController.getComments);
+
 router.get('/discussion/:uuid', async (req, res) => {
     const uuid = req.params.uuid;
     try {
         const discussion = await (discussionController.getDiscussionByUUID(uuid));
         const discussion_post = discussion.post;
         const user = await api.fetchUserData(discussion_post.person_uid);
+        console.log(discussion_post);
+        console.log(user);
         if (!discussion.post) {
             return res.status(404).render('404');
-          }
-        res.render('discussionPost', { discussion_post, user });
+        }
+        const disc_uid = discussion_post.disc_uid;
+        const comments = await fetch (`${BASE_URL}/getComments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ disc_uid })
+        });
+        const disc_comments = await comments.json();
+        console.log(disc_comments.comments);
+        res.render('discussionPost', { discussion_post, user, disc_comments });
     } catch (err) {
         console.error('Error fetching discussion:', err);
         res.status(500).send('Internal Server Error');
     }
 })
 
+router.post('/postComment', discussionController.postComment);
 
 
 module.exports = router;
